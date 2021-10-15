f0a848e54c96d92932cec928295aab31a71546cb
jeanjacquin <jeandanieljacquin@gmail.com>
2021-10-15 16:20:46 +1000

    Some adjustments
    - Altered the search bar to fetch and display data
    - Altered the data.js to sort sentiments into categories

--- app/public/javascripts/search.js
+++ app/public/javascripts/search.js
@@ -1,6 +1,8 @@
 // Get twitter information when searched
+
+const twitterProfileAmount = 5;
+
 let count = 0;
-const streamAmount = 5;
 
 
 const twitterData = (event) => {
@@ -8,23 +10,44 @@ const twitterData = (event) => {
     const searchedTerm = document.getElementById('searchText').value
     document.getElementById('searchText').value = '';
     //console.log(searchEvent.target.textContent);
-    const dummyData = {
-        title: searchedTerm,
-        sentiment: 0.5 + count
-    };
-    console.log(searchedTerm);
-    console.log(count % streamAmount);
-    const chosenDiv = (document.getElementsByClassName('stream')).item(count % streamAmount);
-    while(chosenDiv.firstChild){
-        chosenDiv.removeChild(chosenDiv.firstChild)
+
+    // Some sorta regex thingy here needed? 1 space can't get by, but 2 spaces can
+    // Maybe replace everything but spaces and a-zA-Z, then split on ' ' for multiple queries?
+    if (searchedTerm == 'bad' || searchedTerm == ' '){
+        //
     }
-    const heading = document.createElement("h3");
-    heading.textContent =  dummyData.title;
-    chosenDiv.appendChild(heading);
-    const sentiment = document.createElement("p");
-    sentiment.textContent =  dummyData.sentiment;
-    chosenDiv.appendChild(sentiment);
-    count++;
+    else{
+        fetch(`/data/twitter/${searchedTerm}`)
+        .then( (response) => {
+            return response.json();
+        })
+        .then ((data) => {
+            console.log(data);
+            if (data.source == "No Source"){
+                //
+            }
+            else{
+                const chosenDiv = (document.getElementsByClassName('stream')).item(count % twitterProfileAmount);
+                while(chosenDiv.firstChild){
+                    chosenDiv.removeChild(chosenDiv.firstChild)
+                }
+                const heading = document.createElement("h3");
+                heading.textContent =  data.tagName;
+                chosenDiv.appendChild(heading);
+                const sentiment = document.createElement("p");
+                sentiment.textContent =  'Overall sentiment: ' + data.overallSentiment;
+                chosenDiv.appendChild(sentiment);
+                const importantTerms = document.createElement("p");
+                importantTerms.textContent =  'Important terms: ' + data.importantTerms;
+                chosenDiv.appendChild(importantTerms);
+                count++;
+                //return profileData;
+            }
+        })
+        .catch((error) =>{
+            console.log(error);
+        })
+    }
 }
 
 document.getElementById('searchForm').addEventListener('submit', twitterData);
--- app/routes/data.js
+++ app/routes/data.js
@@ -8,7 +8,15 @@ require('dotenv').config()
 
 // Stopwords retrieved from NLTK library: https://gist.github.com/sebleier/554280
 const stopwords = ['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now']
+
 const maxNumberKeyTerms = 10;
+
+const sentimentLowerLimitVeryPositive = 0.66667;
+const sentimentLowerLimitPositive = 0.33333;
+const sentimentLowerLimitSomewhatPositive = 0;
+const sentimentLowerLimitSomewhatNegative = -0.33333;
+const sentimentLowerLimitNegative = -0.66667;
+const sentimentLowerLimitVeryNegative = -1;
 
 /* GET users listing. */
 router.get('/twitter/:tag', function(req, res) {
@@ -43,15 +51,34 @@ router.get('/twitter/:tag', function(req, res) {
         const groupedTweets = [];
         for (let i = 0; i < twitterParams.count; i++){
             if (tweets.statuses[i] != null){
-                let tweet = tweets.statuses[i].full_text;
-                // Remove any text with RT (retweet)
-                tweet = tweet.replace('RT ', '');
-                // Remove any links
-                tweet = tweet.replace(/(?:https?):\/\/[\n\S]+/g, '')
-                // Lowercase all words.
-                tweet = tweet.toLowerCase();
+                const rawTweet = tweets.statuses[i].full_text;
 
-                groupedTweets.push(tweet); 
+                // Remove any links
+                let cleanTweet = rawTweet.replace(/(?:https?):\/\/[\n\S]+/g, '')
+
+                // Replace ampersands
+                cleanTweet = cleanTweet.replace(/&amp;/g, 'and')
+
+                // Replace newlines with spaces
+                cleanTweet = cleanTweet.replace(/\n/g, ' ')
+
+                cleanTweet = cleanTweet.replace(/RT */g, '')
+
+                // Remove any text with RT (retweet)
+                //let modifiedTweet = rawTweet.replace('RT ', '');
+                // Remove any links
+                //modifiedTweet = modifiedTweet.replace(/(?:https?):\/\/[\n\S]+/g, '')
+                //
+                //modifiedTweet = modifiedTweet.replace(/\n|&amp;/g, ' ')
+                // Lowercase all words.
+                //modifiedTweet = modifiedTweet.toLowerCase();
+
+                const rawTweetObject = {
+                    id: tag + i,
+                    originalTweet: cleanTweet.toLowerCase()
+                }
+
+                groupedTweets.push(rawTweetObject); 
             }
             // If less tweets then expected was found, break out of the loop
             else{
@@ -75,8 +102,12 @@ router.get('/twitter/:tag', function(req, res) {
             // Regular expression, split up words that have 0 or more ':', 0 or more ',' and 1 or more spaces.
             tokenizer = new natural.RegexpTokenizer({pattern: /[^a-z\'\’]+/});
             for (let i = 0; i < groupedTweets.length; i++){
-                const tweetsTokens = tokenizer.tokenize(groupedTweets[i]);
-                groupedTweetsTokens.push(tweetsTokens);
+                const tweetTokens = tokenizer.tokenize(groupedTweets[i].originalTweet);
+                const tokenObject = {
+                    id: groupedTweets[i].id,
+                    tokens: tweetTokens
+                }
+                groupedTweetsTokens.push(tokenObject);
             }
             console.log(groupedTweetsTokens);
             console.log("Tokens got\n");
@@ -84,20 +115,22 @@ router.get('/twitter/:tag', function(req, res) {
 
             // ------------------------ Get sentiments ------------------------  //
             const tweetSentiments = [];
-            const tweetSentimentsWithTweets = []
+            //const tweetSentimentsWithTweets = []
             const Analyser = natural.SentimentAnalyzer;
             const stemmer = natural.PorterStemmer;
             const analyser = new Analyser("English", stemmer, "afinn");
+            //const analyser = new Analyser("English", stemmer, "senticon");
+            //const analyser = new Analyser("English", stemmer, "pattern");
             for (let i = 0; i < groupedTweetsTokens.length; i++){
-                const tweetSentiment = analyser.getSentiment(groupedTweetsTokens[i]);
-                tweetSentiments.push(tweetSentiment);
+                const tweetSentiment = analyser.getSentiment(groupedTweetsTokens[i].tokens);
+                //tweetSentiments.push(tweetSentiment);
                 // Create an object of tweet data
                 // Normally, the array of groupedTweets would be used to get the tweet, but we can't rely it
-                const tweetData = {
-                    tweet: groupedTweetsTokens[i].join(' '),
+                const sentimentObject = {
+                    id: groupedTweetsTokens[i].id,
                     sentiment: tweetSentiment
                 }
-                tweetSentimentsWithTweets.push(tweetData);
+                tweetSentiments.push(sentimentObject);
             }
             console.log(tweetSentiments);
             console.log("Tweet sentiments got\n");
@@ -106,16 +139,18 @@ router.get('/twitter/:tag', function(req, res) {
             // ------------------------ Get important terms ------------------------  //
             const TfIdf = natural.TfIdf;
             const tfidf = new TfIdf();
-            let filteredTweets = [];
+            let tweetsCollated = [];
+            // Search through all the tokens, then add each token to a list of terms if the requirements are met
             for (let i = 0; i < groupedTweetsTokens.length; i++){
-                for (let j = 0; j < groupedTweetsTokens[i].length; j++){
-                    let singularToken = groupedTweetsTokens[i][j]
-                    if (!stopwords.includes(singularToken) && singularToken != tag){
-                        filteredTweets.push(singularToken);
+                for (let j = 0; j < groupedTweetsTokens[i].tokens.length; j++){
+                    let singularToken = groupedTweetsTokens[i].tokens[j]
+                    // Don't include stop words, the user query, or rt (retweet) as possible important terms
+                    if (!stopwords.includes(singularToken) && singularToken != tag && singularToken != 'rt'){
+                        tweetsCollated.push(singularToken);
                     }
                 }
             }
-            tfidf.addDocument(filteredTweets);
+            tfidf.addDocument(tweetsCollated);
             // Get an array of terms, that is sorted by importance
             const twitterFrequentTerms = [];
             const allKeyTerms = tfidf.listTerms(0);
@@ -127,19 +162,89 @@ router.get('/twitter/:tag', function(req, res) {
             console.log('Terms displayed \n');
 
 
-            // ------------------------ Create data object ------------------------  //
+            // ------------------------ Process sentiment data ------------------------  //
             let totalSentiment = 0;
+
+            let veryPositiveSentiments = 0;
+            let positiveSentiments = 0;
+            let somewhatPositiveSentiments = 0;
+            let neutralSentiments = 0;
+            let somewhatNegativeSentiments = 0;
+            let negativeSentiments = 0;
+            let veryNegativeSentiments = 0;
             for (let i = 0; i < tweetSentiments.length; i++){
-                totalSentiment += tweetSentiments[i];
+                const sentimentValue = tweetSentiments[i].sentiment;
+                totalSentiment += sentimentValue;
+                if (sentimentValue == 0){
+                    neutralSentiments++
+                }
+                else if (sentimentValue >= sentimentLowerLimitVeryPositive){
+                    veryPositiveSentiments++;
+                }
+                else if (sentimentValue >= sentimentLowerLimitPositive){
+                    positiveSentiments++;
+                }
+                else if (sentimentValue > sentimentLowerLimitSomewhatPositive){
+                    somewhatPositiveSentiments++;
+                }
+                else if (sentimentValue >= sentimentLowerLimitSomewhatNegative){
+                    somewhatNegativeSentiments++;
+                }
+                else if (sentimentValue >= sentimentLowerLimitNegative){
+                    negativeSentiments++;
+                }
+                else if (sentimentValue >= sentimentLowerLimitVeryNegative){
+                    veryNegativeSentiments++;
+                }
             }
-            const averageSentiment = totalSentiment / tweetSentiments.length;
+
+            const processedSentimentObject = {
+                id: tag,
+                averageSentiment: totalSentiment / tweetSentiments.length,
+                sentimentsVeryPostive: veryPositiveSentiments,
+                sentimentsPositive: positiveSentiments,
+                sentimentsSomewhatPositive: somewhatPositiveSentiments,
+                sentimentsNeutral: neutralSentiments,
+                sentimentsSomewhatNegative: somewhatNegativeSentiments,
+                sentimentsNegative: negativeSentiments,
+                sentimentsVeryNegative: veryNegativeSentiments
+            }
+            //const averageSentiment = totalSentiment / tweetSentiments.length;
+
+
+            // ------------------------ Collate original tweets with sentiments ------------------------  //
+            const allTweets = []
+            for (let i = 0; i < groupedTweets.length; i++){
+                const originalTweet = groupedTweets[i].originalTweet;
+                let tweetSentiment;
+                for (let j = 0; j < tweetSentiments.length; j++){
+                    if (groupedTweets[i].id == tweetSentiments[j].id){
+                        tweetSentiment = tweetSentiments[j].sentiment;
+                        break;
+                    }
+                }
+                const tweetData = {
+                    tweet: originalTweet,
+                    sentiment: tweetSentiment
+                }
+                allTweets.push(tweetData);
+            }
+
+            // ------------------------ Create data object ------------------------  //
 
             const dummyDataV2 = {
                 tagName: tag,
                 source: 'dummy source 2',
-                overallSentiment: averageSentiment,
+                overallSentiment: processedSentimentObject.averageSentiment,
+                sentimentsVeryPostive: processedSentimentObject.sentimentsVeryPostive,
+                sentimentsPositive: processedSentimentObject.sentimentsPositive,
+                sentimentsSomewhatPositive: processedSentimentObject.sentimentsSomewhatPositive,
+                sentimentsNeutral: processedSentimentObject.sentimentsNeutral,
+                sentimentsSomewhatNegative: processedSentimentObject.sentimentsSomewhatNegative,
+                sentimentsNegative: processedSentimentObject.sentimentsNegative,
+                sentimentsVeryNegative: processedSentimentObject.sentimentsVeryNegative,
                 importantTerms: twitterFrequentTerms,
-                tweets: tweetSentimentsWithTweets
+                tweets: allTweets
             };
 
             // ------------------------ Return page with data ------------------------  //
@@ -163,8 +268,8 @@ function GetTwitterParams(query){
     return {
         q: '#' + query,
         lang: 'en',
-        count: 5,
-        result_type: 'popular',
+        count: 100,
+        result_type: 'recent',
         tweet_mode: 'extended'
     };
 }
